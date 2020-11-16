6f3c5fa3e2d0a02e1a3f658b58434c3b2c3c0d19
garethbaars <gareth.baars@intentarchitect.com>
2020-11-16 09:42:15 +0100

    Transferred web client modules here

---
+++ Intent.Modules.Modelers.WebClient/Api/WebClientModel.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Intent.Metadata.Models;
+using Intent.RoslynWeaver.Attributes;
+
+[assembly: DefaultIntentManaged(Mode.Fully)]
+[assembly: IntentTemplate("ModuleBuilder.Templates.Api.ApiPackageModel", Version = "1.0")]
+
+namespace Intent.Modules.Modelers.WebClient.Api
+{
+    [IntentManaged(Mode.Merge)]
+    public class WebClientModel : IHasStereotypes, IMetadataModel
+    {
+        public const string SpecializationType = "Web Client";
+        public const string SpecializationTypeId = "911c35b4-4ba3-404c-a0c6-e5258e53333a";
+
+        [IntentManaged(Mode.Ignore)]
+        public WebClientModel(IPackage package)
+        {
+            if (!SpecializationType.Equals(package.SpecializationType, StringComparison.InvariantCultureIgnoreCase))
+            {
+                throw new Exception($"Cannot create a '{GetType().Name}' from package with specialization type '{package.SpecializationType}'. Must be of type '{SpecializationType}'");
+            }
+
+            UnderlyingPackage = package;
+        }
+
+        public IPackage UnderlyingPackage { get; }
+        public string Id => UnderlyingPackage.Id;
+        public string Name => UnderlyingPackage.Name;
+        public IEnumerable<IStereotype> Stereotypes => UnderlyingPackage.Stereotypes;
+        public string FileLocation => UnderlyingPackage.FileLocation;
+
+    }
+}
---
+++ Intent.Modules.Modelers.WebClient/ApiMetadataPackageExtensions.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.Linq;
+using Intent.Metadata.Models;
+using Intent.RoslynWeaver.Attributes;
+
+[assembly: DefaultIntentManaged(Mode.Fully)]
+[assembly: IntentTemplate("ModuleBuilder.Templates.Api.ApiMetadataPackageExtensions", Version = "1.0")]
+
+namespace Intent.Modules.Modelers.WebClient.Api
+{
+    public static class ApiMetadataPackageExtensions
+    {
+        public static IList<WebClientModel> GetWebClientModels(this IDesigner designer)
+        {
+            return designer.GetPackagesOfType(WebClientModel.SpecializationTypeId)
+                .Select(x => new WebClientModel(x))
+                .ToList();
+        }
+
+
+    }
+}
