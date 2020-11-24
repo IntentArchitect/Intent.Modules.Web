c623f9444978d27f2f771eed51d340a7dee551d3
garethbaars <gareth.baars@intentarchitect.com>
2020-11-24 20:22:50 +0100

    updates to angular and designer mapping configuration

---
+++ Intent.Modules.Angular.Layout/AngularAppExtensionsModel.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Intent.Metadata.Models;
+using Intent.Modelers.WebClient.Api;
+using Intent.Modules.Angular.Layout.Api;
+using Intent.RoslynWeaver.Attributes;
+
+[assembly: DefaultIntentManaged(Mode.Fully)]
+[assembly: IntentTemplate("ModuleBuilder.Templates.Api.ApiPackageExtensionModel", Version = "1.0")]
+
+namespace Intent.Modules.Angular.Layout
+{
+    [IntentManaged(Mode.Merge)]
+    public class AngularAppExtensionsModel : WebClientModel
+    {
+        [IntentManaged(Mode.Ignore)]
+        public AngularAppExtensionsModel(IPackage package) : base(package)
+        {
+        }
+
+        public IList<FormControlModel> FormControlTypes => UnderlyingPackage.ChildElements
+            .Where(x => x.SpecializationType == FormControlModel.SpecializationType)
+            .Select(x => new FormControlModel(x))
+            .ToList();
+    }
+}
