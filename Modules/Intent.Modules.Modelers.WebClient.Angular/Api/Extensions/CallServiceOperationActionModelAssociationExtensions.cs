ca3fde661a3bed05b7ff3a367d1f31ce27ccdb39
Gareth Baars <gareth.baars@intentarchitect.com>
2024-03-29 06:32:06 +0100

    Deprecating WebClient

---
+++ Modules/Intent.Modules.Modelers.WebClient.Angular/Api/Extensions/CallServiceOperationActionModelAssociationExtensions.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using System.Linq;
+using Intent.Modelers.Types.ServiceProxies.Api;
+using Intent.RoslynWeaver.Attributes;
+
+[assembly: DefaultIntentManaged(Mode.Fully)]
+[assembly: IntentTemplate("Intent.ModuleBuilder.Templates.Api.ApiAssociationModelExtensions", Version = "1.0")]
+
+namespace Intent.Modelers.WebClient.Angular.Api
+{
+    public static class CallServiceOperationActionModelAssociationExtensions
+    {
+        [IntentManaged(Mode.Fully)]
+        public static IList<CallServiceOperationActionTargetEndModel> CallServiceOperationActionTargets(this ComponentModel model)
+        {
+            return model.InternalElement.AssociatedElements
+                .Where(x => x.Association.SpecializationType == CallServiceOperationActionModel.SpecializationType && x.IsTargetEnd())
+                .Select(x => CallServiceOperationActionModel.CreateFromEnd(x).TargetEnd)
+                .ToList();
+        }
+
+        [IntentManaged(Mode.Fully)]
+        public static IList<CallServiceOperationActionTargetEndModel> CallServiceOperationActionTargets(this ComponentCommandModel model)
+        {
+            return model.InternalElement.AssociatedElements
+                .Where(x => x.Association.SpecializationType == CallServiceOperationActionModel.SpecializationType && x.IsTargetEnd())
+                .Select(x => CallServiceOperationActionModel.CreateFromEnd(x).TargetEnd)
+                .ToList();
+        }
+
+        [IntentManaged(Mode.Fully)]
+        public static IList<CallServiceOperationActionSourceEndModel> CallServiceOperationActionSources(this OperationModel model)
+        {
+            return model.InternalElement.AssociatedElements
+                .Where(x => x.Association.SpecializationType == CallServiceOperationActionModel.SpecializationType && x.IsSourceEnd())
+                .Select(x => CallServiceOperationActionModel.CreateFromEnd(x).SourceEnd)
+                .ToList();
+        }
+
+    }
+}
