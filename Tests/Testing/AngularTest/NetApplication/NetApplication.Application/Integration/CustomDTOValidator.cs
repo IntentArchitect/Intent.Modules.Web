9b8eb5474d000489d7d7284a0518197983e5ee5d
Jonathan Lydall <jonathan.lydall@intentarchitect.com>
2024-07-29 23:08:05 +0200

    PROMOTED MODULES TO FINAL RELEASE

--- Tests/Testing/AngularTest/NetApplication/NetApplication.Application/Integration/CustomDTOValidator.cs
+++ Tests/Testing/AngularTest/NetApplication/NetApplication.Application/Integration/CustomDTOValidator.cs
@@ -7,7 +7,7 @@ using Intent.RoslynWeaver.Attributes;
 
 namespace NetApplication.Application.Integration
 {
-    [IntentManaged(Mode.Merge, Signature = Mode.Fully)]
+    [IntentManaged(Mode.Fully, Body = Mode.Merge)]
     public class CustomDTOValidator : AbstractValidator<CustomDTO>
     {
         [IntentManaged(Mode.Merge)]
@@ -16,7 +16,6 @@ namespace NetApplication.Application.Integration
             ConfigureValidationRules();
         }
 
-        [IntentManaged(Mode.Fully)]
         private void ConfigureValidationRules()
         {
             RuleFor(v => v.ReferenceNumber)
